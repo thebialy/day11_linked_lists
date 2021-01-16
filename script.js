8072b976714092f6205a809edbd64137714c3af8
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 10:04:37 -0600

    leet code linkedlist question complete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
 var addTwoNumbers = function(l1, l2) {
     let result = new ListNode(0)
     let currentNode = result
@@ -11,8 +13,14 @@ var addTwoNumbers = function(l1, l2) {
         let sum = v1 + v2 + carryOver
         carryOver = Math.floor(sum / 10)
         sum = sum % 10
+        currentNode.next = new ListNode(sum)
 
+        currentNode = currentNode.next
         if(l2 != null) l1 = l1.next
         if(l2 != null) l2 = l2.next
+    }
+
+    if (carryOver > 0) {
+        currentNode.next = new ListNode(carryOver)
     }
 }
