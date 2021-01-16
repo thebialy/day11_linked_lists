ad7d2fb6ccdcc0ec901093a1f68254af75fc9f15
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 08:51:02 -0600

    sum added

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ const addTwoNumbers = (l1, l2) => {
         const v2 = l2.val
         if (l1 != null) v1 = l1.val
         if (l2 != null) v2 = l2.val
+
+        const sum = v1 + v2
+
         if(l2 != null) l1 = l1.next
         if(l2 != null) l2 = l2.next
     }
