4b99a46b8be618ef1dab001e5c4775a11529cf36
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 08:52:33 -0600

    fixed variables and add empty array to push sum into

--- script.js
+++ script.js
@@ -9,13 +9,15 @@
 // @return {ListNode}
 
 const addTwoNumbers = (l1, l2) => {
+    const a =[]
     while(l1 != null || l2 != null) {
-        const v1 = l1.val
-        const v2 = l2.val
+        const v1 = 0
+        const v2 = 0
         if (l1 != null) v1 = l1.val
         if (l2 != null) v2 = l2.val
 
         const sum = v1 + v2
+        a.push(sum)
 
         if(l2 != null) l1 = l1.next
         if(l2 != null) l2 = l2.next
