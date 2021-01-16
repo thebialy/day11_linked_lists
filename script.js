8aa42bb375a349a69de74c85e648f74dc105f3e7
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 08:41:19 -0600

    starting linked list lesson

---
+++ script.js
@@ -0,0 +1,14 @@
+// Defintion for singly - linked list.
+// class ListNode(val) {
+//     this.val = val;
+//     this.next = null;
+// }
+
+// @param {ListNode} l1
+// @param {ListNode} l2
+// @return {ListNode}
+
+const addTwoNumbers = (l1, l2) => {
+
+  
+}
