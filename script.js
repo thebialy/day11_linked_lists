9581b0beceeafa11763615186046c134e9389e40
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 09:31:52 -0600

    added carryover

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// Defintion for singly - linked list.
-// class ListNode(val) {
-//     this.val = val;
-//     this.next = null;
-// }
-
-// @param {ListNode} l1
-// @param {ListNode} l2
-// @return {ListNode}
-
 var addTwoNumbers = function(l1, l2) {
     const a =[]
+    let carryOver = 0
     while(l1 != null || l2 != null) {
         let v1 = 0
         let v2 = 0
         if (l1 != null) v1 = l1.val
         if (l2 != null) v2 = l2.val
 
-        const sum = v1 + v2
+        const sum = v1 + v2 + carryOver
         a.push(sum)
 
         if(l2 != null) l1 = l1.next
