63db3da5bcaa46721519b73240e57b5dcf0d55b8
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 09:25:09 -0600

    had to change const to let

--- script.js
+++ script.js
@@ -8,11 +8,11 @@
 // @param {ListNode} l2
 // @return {ListNode}
 
-const addTwoNumbers = (l1, l2) => {
+var addTwoNumbers = function(l1, l2) {
     const a =[]
     while(l1 != null || l2 != null) {
-        const v1 = 0
-        const v2 = 0
+        let v1 = 0
+        let v2 = 0
         if (l1 != null) v1 = l1.val
         if (l2 != null) v2 = l2.val
 
@@ -22,4 +22,5 @@ const addTwoNumbers = (l1, l2) => {
         if(l2 != null) l1 = l1.next
         if(l2 != null) l2 = l2.next
     }
+    console.log(a)
 }
