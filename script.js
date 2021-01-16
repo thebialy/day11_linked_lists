6b6c1288318c90fbfe6ed0311ae4fd329019b233
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-16 08:50:03 -0600

    set up if statements so in case 2 linked list are different lengths

--- script.js
+++ script.js
@@ -10,8 +10,11 @@
 
 const addTwoNumbers = (l1, l2) => {
     while(l1 != null || l2 != null) {
+        const v1 = l1.val
+        const v2 = l2.val
+        if (l1 != null) v1 = l1.val
+        if (l2 != null) v2 = l2.val
         if(l2 != null) l1 = l1.next
-        if(l2 !=)
-
+        if(l2 != null) l2 = l2.next
     }
 }
